72b919353539bbc7e3a1894d8ff42221e5ccd29b
gilvinda dsouza <gilvindadsouza@gmail.com>
2024-01-08 09:47:22 +0530

    first github commit

---
+++ sybcaproject140/src/module-info.java
@@ -0,0 +1,2 @@
+module sybcaproject140 {
+}
---
+++ sybcaproject140/src/sybcaproject140/clsgithub.java
@@ -0,0 +1,11 @@
+package sybcaproject140;
+
+public class clsgithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world ");
+	}
+
+}
