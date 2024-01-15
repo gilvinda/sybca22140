8747d1d6debea8ee895dd0524ec6197a36430c7c
gilvinda dsouza <gilvindadsouza@gmail.com>
2024-01-15 10:04:52 +0530

    commit

---
+++ sybcaproject140/src/sybcaproject140/add2nos.java
@@ -0,0 +1,19 @@
+package sybcaproject140;
+
+import java.util.Scanner;
+public class add2nos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan=new Scanner(System.in);
+		int no1,no2,sum;
+		System.out.println("enter no 1");
+		no1=scan.nextInt();
+		System.out.println("enter no 2");
+		no2=scan.nextInt();
+		sum=no1+no2;
+		System.out.println("sum is "+sum);
+	}
+
+}
